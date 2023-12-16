3bbf083f01263e40057288be1cc4dfcd5bf6effb
Aaron Santana <system@savaldev.com>
2023-12-15 18:52:48 -0600

    solucion ejercicio 2

--- entrada-al-bar/main.py
+++ entrada-al-bar/main.py
@@ -4,13 +4,47 @@ def main():
 
 
     # Escribe acà el còdigo, suerte :)
-    nombre = input("Ingresa tu nombre: ")       
-    if nombre in black_list:
-        print("Lo siento, no puedes entrar. Estas en la lista negra.")
-    else:
-        edad = int(input("Ingresa tu edad: "))
-        if edad < 18:
-            print("Lo siento, no puedes entrar. Debes ser mayor de 18 años.")
-        else:
-            print("¡Bienvenido! Puedes entrar al bar.")
+    verificados = []
+
+    while True:
+        print("1. Editar lista negra.")
+        print("2. Ver lista negra.")
+        print("3. Verificar entrada.")
+        print("4. Ver personas verificadas.")
+        print("5. Número de personas que han entrado al bar")
+        print("6. Terminar")
+        accion = int(input("Ingrese el número de la accion que desea realizar: "))
+
+        if accion == 1:
+            edit = int(input("1. Agregar, 2. Eliminar: "))
+            name = input("Nombre: ")
+            if edit == 1:
+                black_list.append(name)
+            elif edit == 2:
+                black_list.remove(name)
+
+        elif accion == 2:
+            print(black_list)
+        
+        elif accion == 3:
+            nombre = input("Ingresa tu nombre: ")
+            verificados.append(nombre)               
+            if nombre in black_list:
+                print("Lo siento, no puedes entrar. Estas en la lista negra.")
+            else:
+                edad = int(input("Ingresa tu edad: "))
+                if edad < 18:
+                    print("Lo siento, no puedes entrar. Debes ser mayor de 18 años.")
+                else:
+                    print("¡Bienvenido! Puedes entrar al bar.")
+
+        elif accion == 4:
+            print(verificados)
+
+        elif accion == 5:
+            print(len(verificados))
+
+        elif accion == 6:
+            break
+
 main() 
